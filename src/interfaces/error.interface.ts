afc1e55ffdffe7042dfba395318a9f5229f7de60
Rafael Santos <rafaelcs_94@hotmail.com>
2022-06-09 16:05:09 -0300

    feat: adds error class

---
+++ src/interfaces/error.interface.ts
@@ -0,0 +1,11 @@
+export default class ErrorHandler extends Error {
+  status: number;
+
+  message: string;
+
+  constructor(status: number, message: string) {
+    super();
+    this.status = status;
+    this.message = message;
+  }
+}
