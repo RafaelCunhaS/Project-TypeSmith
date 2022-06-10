7ce6943d6e87078da4ff82e16415cf4a4b279d06
Rafael Santos <rafaelcs_94@hotmail.com>
2022-06-10 10:24:29 -0300

    feat: adds new types for User

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,11 @@
-export interface User {
+export interface UserWithId {
+  id: number,
   username: string,
   classe: string,
   level: number,
   password: string
 }
+
+export type User = Omit<UserWithId, 'id'>;
+
+export type LoginUser = Omit<User, 'classe' | 'level'>;
